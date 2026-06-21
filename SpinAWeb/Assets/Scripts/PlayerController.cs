35d9ef45cf80cecdeba5074f4c2179cf72324b95
tokkebee <oliverk13905@gmail.com>
2026-06-21 17:19:17 -0400

    Spider Stays on Screen
    changed player controller script to make sure the spider cant wander off screen

--- SpinAWeb/Assets/Scripts/PlayerController.cs
+++ SpinAWeb/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] public Camera camera;
     [SerializeField] private float speed;
 
+    [SerializeField] private Vector2 screenBounds;
+
     void Start()
     {
         camera = Camera.main;
+        //screenBounds = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        screenBounds.y = camera.orthographicSize;
+        screenBounds.x = screenBounds.y * camera.aspect;
+
+        Debug.Log(screenBounds);
     }
 
     void Update() {
@@ -19,18 +26,35 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        Vector3 mousePos = camera.ScreenToWorldPoint(
-            new Vector3(
-                Input.mousePosition.x,
-                Input.mousePosition.y,
-                -camera.transform.position.z
-            )
+        // Vector3 mousePos = camera.ScreenToWorldPoint(
+        //     new Vector3(
+        //         Input.mousePosition.x,
+        //         Input.mousePosition.y,
+        //         -camera.transform.position.z
+        //     )
+        // );
+        // mousePos.z = transform.position.z;
+
+        // Vector3 dir = (mousePos - transform.position).normalized;
+
+        // transform.position += dir * speed * Time.deltaTime;
+
+        Vector3 newPos = transform.position + transform.up * speed * Time.deltaTime;
+
+        float depth = transform.position.z - camera.transform.position.z;
+
+        Vector3 min = camera.ScreenToWorldPoint(
+            new Vector3(0, 0, depth)
         );
-        mousePos.z = transform.position.z;
 
-        Vector3 dir = (mousePos - transform.position).normalized;
+        Vector3 max = camera.ScreenToWorldPoint(
+            new Vector3(Screen.width, Screen.height, depth)
+        );
 
-        transform.position += dir * speed * Time.deltaTime;
+        newPos.x = Mathf.Clamp(newPos.x, min.x, max.x);
+        newPos.y = Mathf.Clamp(newPos.y, min.y, max.y);
+
+        transform.position = newPos;
     }
 
     //Deposits web
